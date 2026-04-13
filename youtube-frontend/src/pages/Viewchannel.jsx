405013ec4b4dde939d641b3001f23d205e41fdd1
Gaurav Bhardwaj <gau.bwj@gmail.com>
2026-04-13 21:36:37 +0530

    stying of particualr channel page

--- youtube-frontend/src/pages/Viewchannel.jsx
+++ youtube-frontend/src/pages/Viewchannel.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import '../css/viewchannel.css'
 
 function Viewchannel(){
     const {id}=useParams();
