0940b7906acf52b9842a8f03a629bfdfd3db94db
Gaurav Bhardwaj <gau.bwj@gmail.com>
2026-04-13 21:44:09 +0530

    styuling of uploading a video. its a form. logic was alredy done. jsut done the css

--- youtube-frontend/src/pages/Uploadvideo.jsx
+++ youtube-frontend/src/pages/Uploadvideo.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
+import '../css/Uploadvideo.css';
 
 function Uploadvideo(){
     const [title,setTitle]=useState("");
